88446cf48c563f7dadbfd4cc97740be6033327c5
Assaf Kabesa <asafka73@gmail.com>
2022-11-03 16:24:49 +0200

    made the email course asking work(need to add real courses tho)

--- WebApplication2/WebApplication2/Controllers/CoursesController.cs
+++ WebApplication2/WebApplication2/Controllers/CoursesController.cs
@@ -22,5 +22,49 @@ namespace WebApplication2.Controllers
             return View(model);
         }
 
+        public ActionResult MailCourses(string email_address,string course_id)
+        {
+            int course_id_num = -1;
+            if(!int.TryParse(course_id, out course_id_num))
+            {
+                course_id_num = -1;
+            }
+            
+            if(email_address != "" && course_id_num != -1)
+            {
+                const string subject = "New Request!";
+                string body = "You choose to get information about the course{" + course_id_num.ToString() + "}";
+
+
+
+
+
+                string username = "asafka73@gmail.com"; // high lvl sec 
+                string password = "boodnletuoiyhpsk";
+                ICredentialsByHost credentials = new NetworkCredential(username,password);
+
+                SmtpClient smtpClient = new SmtpClient()
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    Credentials = credentials
+                };
+
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(username);
+                mail.To.Add(email_address);
+                mail.Subject = subject;
+                mail.Body = body;
+
+                smtpClient.Send(mail);
+                return RedirectToAction("index","Articles");
+            }
+            else
+            {
+                return RedirectToAction("index","Courses");
+            }
+        }
+
     }
 }
